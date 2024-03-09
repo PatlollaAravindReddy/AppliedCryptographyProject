1b36b8b03b4006d05321402feadf976b3dc5b02b
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2024-03-09 01:13:58 -0600

    Initial commit

---
+++ demo/src/main/java/com/example/demo/AppliedCryptographyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliedCryptographyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliedCryptographyProjectApplication.class, args);
+	}
+
+}
